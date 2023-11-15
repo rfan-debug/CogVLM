70a5b0c339ada201a1321c1df9c6f529a71cad09
Ruixi Fan <ruixi@treat.xyz>
2023-11-15 00:02:25 -0800

    rerun

--- api_demo.py
+++ api_demo.py
@@ -125,20 +125,16 @@ def call_predict(
             input_texts = [input_text]
             pil_imgs = [pil_img]
 
-
-
             print("result_text: ", result_text)
 
             if WORLD_SIZE > 1:
-                if RANK == 0:
-                    torch.distributed.broadcast_object_list(input_texts, src=0)
-                    if len(result_text) > 0:
-                        torch.distributed.broadcast_object_list(result_text, src=0)
-                    torch.distributed.broadcast_object_list(image_prompts, src=0)
-                    torch.distributed.broadcast_object_list(pil_imgs, src=0)
-                # else:
-                #     while True:
-                #         print("Waiting for broadcasting from source device")
+                print(f"broadcasting at {RANK}")
+                torch.distributed.broadcast_object_list(input_texts, src=0)
+                if len(result_text) > 0:
+                    torch.distributed.broadcast_object_list(result_text, src=0)
+                torch.distributed.broadcast_object_list(image_prompts, src=0)
+                torch.distributed.broadcast_object_list(pil_imgs, src=0)
+                print(f"broadcasting finished at {RANK}")
 
 
                 print(f"image_prompts: {image_prompts}")
