34bc849ac57e434918904f550d2737ed89dd04d4
Ruixi Fan <ruixi@treat.xyz>
2023-11-14 22:45:08 -0800

    cool

--- api_starter.py
+++ api_starter.py
@@ -6,5 +6,5 @@ import uvicorn
 RANK = int(os.environ.get('RANK', 0))
 
 if __name__ == "__main__":
-    uvicorn.run("your_app_module:app", host="0.0.0.0", port=5000 + RANK)
+    uvicorn.run("fastapi_example:app", host="0.0.0.0", port=5000 + RANK)
 
